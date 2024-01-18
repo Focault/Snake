df98ef0c7808a58f0a96e117b619c5445c4ee051
Focault <96660268+Focault@users.noreply.github.com>
2024-01-18 23:11:38 +0200

    Now sleeps until next frame instead of continous polling

--- inc/inl/sf_time.hxx
+++ inc/inl/sf_time.hxx
@@ -1,6 +1,9 @@
 #ifndef SF_TIME_HXX
 #define SF_TIME_HXX
 
+#include <SFML/Graphics.hpp>
+#include <unistd.h>
+
 namespace sfml {
 
 inline Time::Time()
@@ -8,12 +11,15 @@ inline Time::Time()
 {}
 
 inline bool Time::isFrame(float a_fps) noexcept {
-    constexpr int32_t milisecondsInSeconds = 1000;
+    constexpr int32_t milisecondsInSecond = 1000;
+    constexpr int32_t microsecondsInMilisecond = 1000;
     sf::Time elapsed = m_clock.getElapsedTime();
-    if (elapsed.asMilliseconds() >= (milisecondsInSeconds / a_fps)) {
+    int32_t milisecondsPerFrame = milisecondsInSecond / a_fps;
+    if (elapsed.asMilliseconds() >= milisecondsPerFrame) {
         m_clock.restart();
         return true;
     }
+    usleep((milisecondsPerFrame - elapsed.asMilliseconds()) * microsecondsInMilisecond);
     return false;
 }
 
